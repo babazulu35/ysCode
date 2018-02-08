4bf169efbe8f09d5fd1776d387a5d4cc730c65cb
Hakan Huriyet <aysanhakan@gmail.com>
2018-02-08 19:37:45 +0300

    search servis kurgulandı

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 (function() {
 
 
+    var serchResult = searchService.searchResultData();
+
 
     var pageObjects = uiService.getPageObjects();
     document.querySelector(pageObjects.searchButton).addEventListener('click', function() {
@@ -15,21 +17,20 @@
 
     })
 
-    pubSub.subscribe("jsonData", function(data) {
+    pubSub.subscribe("searchResult", function(data) {
         var html, newHtml;
-        var resultPath = data.d.ResultSet || '';
+        console.log(data);
         html = '<li id="%ProductId%"><div class="searchResult"><div class="avatar"> <img src="https://picsum.photos/g/100/?random" alt=""></div><div class="info"><span class="product-title">%ProductName%</span><div class="product-detail"> %Description% </div><div class="product-price"><span>Fiyatı: %ProductPrice%</span></div><div class="add-product"><i class="fas  fa-minus"></i> <div data-quantity="20"></div><i class="fas fa-plus"></i><button id="%ButtonId%" class="add_to_basket button">EKLE</button></div></div></div></li>';
-        for (var i = 0; i < resultPath.length; i++) {
-            for (var a = 0; a < resultPath[i].Products.length; a++) {
-                newHtml = html.replace('%ProductName%', resultPath[i].Products[a].DisplayName);
-                newHtml = newHtml.replace('%ProductId%', resultPath[i].Products[a].ProductId);
-                newHtml = newHtml.replace('%ButtonId%', resultPath[i].Products[a].ProductId);
-                newHtml = newHtml.replace('%Description%', resultPath[i].Products[a].Description);
-                newHtml = newHtml.replace('%ProductPrice%', resultPath[i].Products[a].ListPrice)
-                document.getElementById('searchResult').innerHTML += newHtml;
-            }
-
+        for (var a = 0; a < data.length; a++) {
+            newHtml = html.replace('%ProductName%', data[a].DisplayName);
+            newHtml = newHtml.replace('%ProductId%', data[a].ProductId);
+            newHtml = newHtml.replace('%ButtonId%', data[a].ProductId);
+            newHtml = newHtml.replace('%Description%', data[a].Description);
+            newHtml = newHtml.replace('%ProductPrice%', data[a].ListPrice)
+            document.getElementById('searchResult').innerHTML += newHtml;
         }
+
+
 
         document.querySelector(pageObjects.addToBasket).addEventListener('click', function() {
             console.log(this.getAttribute('id'));
@@ -38,6 +39,7 @@
         })
 
     });
+
 
 
 
--- app/services/searchService.js
+++ app/services/searchService.js
@@ -2,19 +2,51 @@ var searchService = (function(jsonParser) {
 
 
     var data = jsonParser.rawData();
-
-
-
-    pubSub.subscribe('searchInputValue', function(data) {
-        alert(data.searchTypeValue);
-    })
-
-
+    var searchWord = '';
 
 
     pubSub.subscribe('jsonData', function(data) {
-        console.log("Json DAta", data);
+        pubSub.subscribe('searchInputValue', function(searchData) {
+            alert(searchData.searchTypeValue);
+            console.log("On Searc Result");
+            console.log(searchData.searchTypeValue);
+            var resultPath = data.d.ResultSet || '';
+            for (var i = 0; i < resultPath.length; i++) {
+                for (var a = 0; a < resultPath[i].Products.length; a++) {
+                    //Seçilmiş Menü (Kebap)
+                    var rgxp = new RegExp(searchData.searchTypeValue, "g");
+                    if (resultPath[i].Products[a].DisplayName.match(rgxp)) {
+                        console.log("Event");
+                        console.log("Burası", resultPath[i].Products[a].DisplayName);
+                        pubSub.unsubscribe('jsonData', data);
+                        pubSub.emit('searchResult', resultPath[i].Products[a])
+                    } else {
+                        console.log("Kayıt Yok");
+                    }
+
+                }
+            }
+        })
+
     })
+
+
+
+
+    var searchResult = function() {
+
+
+    }
+
+    return {
+        searchResultData: searchResult
+    }
+
+
+
+
+
+
 
 
 })(jsonParser)
